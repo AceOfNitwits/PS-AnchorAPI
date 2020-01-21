663803d0d8f4cc23dc0edec32f3c111b86eb70aa
AceOfNitwits <59476593+AceOfNitwits@users.noreply.github.com>
2020-01-21 17:40:49 -0500

    Edited to match module function names.

--- Anchor-BackupCheck.ps1
+++ Anchor-BackupCheck.ps1
@@ -1,43 +1,50 @@
 ﻿# Include the Anchor API scripts
-. ".\Anchor-ApiOauth.ps1"
-. ".\Anchor-ApiReporting.ps1"
+#. ".\Anchor-ApiOauth.ps1"
+#. ".\Anchor-ApiReporting.ps1"
 
-$anchorParentOrgID = 361 # Modify this to be your own org_id
+#$anchorParentOrgID = 361 # Modify this to be your own org_id
 
-#Run this first to get an Oauth token.
-Function Authenticate {
-    Get-AnchorOauthToken
+#Run this first to get an Oauth token and top organization id
+Function Sign-In {
+    Authenticate-AnchorAccount
 }
 
 # Populates various lists of anchor objects 
 Function Populate-UsefulVariables{
-    Write-Host "Getting AnchorOrg object for the parent org id..."
-    $script:anchorParentOrg = Get-AnchorOrg -id $anchorParentOrgID
+
+    Write-Host "Getting top-level org..."
+    $script:anchorTopOrg = Get-AnchorOrg -Top
+    Write-Host "Top-level org id:" $script:anchorTopOrg.id
     
-    Write-Host "Getting collection of AnchorOrg objects for children of parent org..."
-    $script:anchorCustomerOrgs = $anchorParentOrg | Get-AnchorOrgChildren
+    Write-Host "Getting collection of AnchorOrg objects for children of top org..."
+    $script:anchorCustomerOrgs = $anchorTopOrg | Get-AnchorOrgChildren
     Write-Host $script:anchorCustomerOrgs.Count "customer orgs retrieved."
 
-    Write-Host "Getting collection of AnchorMachine objects for the parent org..."
-    # Note that the parent org owns all child org machines, so this includes all machines.
-    $script:anchorMachines = $anchorParentOrg | Get-AnchorOrgMachines
+    $script:anchorOrgs = @($script:anchorTopOrg) + $script:anchorCustomerOrgs
+
+    Write-Host "Getting collection of AnchorMachine objects for the top org..."
+    Write-Host "Note that an org also owns all child org machines, so this includes all machines in your anchor environment." -ForegroundColor Yellow
+    $script:anchorMachines = $anchorTopOrg | Get-AnchorOrgMachines
     Write-Host $script:anchorMachines.Count "machines retrieved."
 
     Write-Host "Getting collection of AnchorMachine objects for the customer orgs..."
-    # Note that the parent org owns all child org machines, so this includes all machines.
+    Write-Host "Note that this does not include machines explicity in the parent org, so we'll have to calculate those and add them back in later." -ForegroundColor Yellow
     $script:anchorCustomerMachines = $anchorCustomerOrgs | Get-AnchorOrgMachines
     Write-Host $script:anchorCustomerMachines.Count "machines retrieved."
 
-    Write-Host "Calculating collection of AnchorMachine objects explicitly in the parent org..."
-    $Script:anchorParentMachines = $anchorParentOrg | Get-AnchorOrgMachines -ExcludeChildren
+    Write-Host "Calculating collection of AnchorMachine objects explicitly in the top org..."
+    Write-Host "This is not a feature of the API, but it's implemented as part of the module function." -ForegroundColor Yellow
+    $Script:anchorParentMachines = $anchorTopOrg | Get-AnchorOrgMachines -ExcludeChildren
     Write-Host $script:anchorParentMachines.Count "machines calculated."
 
     Write-Host "Creating collection of all AnchorMachine objects with their correct organization_id..."
+    Write-Host "We need to add the two lists of machines together now." -ForegroundColor Yellow
     $Script:anchorOrgMachines = $Script:anchorParentMachines + $Script:anchorCustomerMachines
-    Write-Host $script:anchorOrgMachines.Count "machines in total. (This should match the number of machines retrieved from the parent org.)"
+    Write-Host $script:anchorOrgMachines.Count "machines in total. (This should match the number of machines retrieved from the top-level org.)"
     
-    Write-Host "Getting collection of AnchorRoot objects for the parent org..."
-    $script:anchorRoots = $anchorParentOrg | Get-AnchorOrgRoots
+    Write-Host "Getting collection of AnchorRoot objects for the top org..."
+    Write-Host "This retrieves a list of all roots in your Anchor environment." -ForegroundColor Yellow
+    $script:anchorRoots = $anchorTopOrg | Get-AnchorOrgRoots
     Write-Host $script:anchorRoots.Count "roots retrieved."
 }
 
@@ -45,7 +52,7 @@ Function Populate-UsefulVariables{
 Function Get-AllBackups {
     Write-Host "Getting collection of backups for all AnchorMachine objects..."
     # Getting backup roots by org doesn't tell you which machines they're associated with, so we have to itterate through all machines to see if they have backups. 😒
-    # Excluding mobile machine_type cuts down some of the work.
+    # Excluding machine_type 'mobile' cuts down some of the work.
     # It takes about 0.3 seconds per machine. Using Measure-Command gives some mildly interesting data about how long it takes.
     $duration = Measure-Command{$script:anchorBackups = $anchorMachines | Where-Object machine_type -ne mobile | Get-AnchorMachineBackups}
     $secondsPerMachine = $duration.TotalSeconds / ($anchorMachines | Where-Object machine_type -ne mobile).Count
@@ -65,20 +72,24 @@ Function Get-BackupsLastModified {
     Write-Host "$secondsPerBackup seconds per backup root."
 }
 
+# Running this function runs all of the above.
 Function Report-MachineBackups{
     Populate-UsefulVariables
     Get-AllBackups
     Get-BackupsLastModified
-    $anchorMachines | ? machine_type -ne mobile | ForEach-Object {
-        $machineId = $_.id
-        $machineName = $_.dns_name
-        $myBackups = $anchorBackups | ? {$_.machine_id -eq $machineId}
-        $myBackups | ForEach-Object {
-            $rootId = $_.id
-            $myLastModified = ($Script:anchorBackupsLastModified | ? {$_.id -eq $rootId}).modified
-            If($myLastModified){
-                $_ | Select @{'N'='machine_name';'E'={"$machineName"}}, path, @{'N'='last_modified';'E'={"$myLastModified"}}
-            }
-        }
+    $script:backupReport = [pscustomobject]@()
+    $anchorBackups | ForEach-Object {
+        $rootId = $_.id
+        $machineId = $_.machine_id
+        $path = $_.path
+        $myLastModified = ($Script:anchorBackupsLastModified | ? {$_.id -eq $rootId}).modified
+        $myMachine = $script:anchorOrgMachines | ? {$_.id -eq $machineId}
+        $myMachineName = $myMachine.dns_name
+        $myMachineOrg = $myMachine.organization_id
+        $myOrgName = ($script:anchorOrgs | ? {$_.id -eq $myMachineOrg}).name
+        $Script:backupReport += [pscustomobject]@{'Organization'="$myOrgName";'Machine'="$myMachineName";'BackupPath'="$path";'LastModified'="$myLastModified"}
+    }
+    $script:backupReport | ForEach-Object {
+        Write-Host ($_ | Out-String)
     }
 }
