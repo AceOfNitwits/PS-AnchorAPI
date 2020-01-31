b03652ada73d2c5bd5556ca3a0649b587b3a33ff
AceOfNitwits <59476593+AceOfNitwits@users.noreply.github.com>
2020-01-31 13:59:40 -0500

    Add files via upload

---
+++ Examples/Get-AllMachinesWithCorrectCompanyId.ps1
@@ -0,0 +1,15 @@
+﻿# To use this function, run this script, then run something like the following:
+# get-anchororg -top | Get-OrgAndChildMachinesByOrg
+
+Function Get-OrgAndChildMachinesByOrg {
+# Returns a list of all machines in the organiztion and all child organizations, with the correct company_id
+    [CmdletBinding()]
+    param([Parameter(ValueFromPipeline)]$InputObject)
+    process{
+        # Return all the machines in this organization, excluding machines from child orgs.
+        $_ | Get-AnchorOrgMachines -ExcludeChildren
+        # Get the list of child orgs and pass them to this function recursively to return the results.
+        $_ | Get-AnchorOrgChildren | Get-OrgAndChildMachinesByOrg
+    }
+
+}
